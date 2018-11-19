bc9de3698832d3569819462365464171e15b16ca
huohuo <2991297756@qq.com>
2018-11-19 01:11:28 -0800

    add chapter 3.4.3 exchange function

---
+++ chapter_3_4_3/exchange.s
@@ -0,0 +1,17 @@
+	.file	"exchange.c"
+	.text
+	.globl	exchange
+	.type	exchange, @function
+exchange:
+.LFB0:
+	.cfi_startproc
+	movl	4(%esp), %edx
+	movl	(%edx), %eax
+	movl	8(%esp), %ecx
+	movl	%ecx, (%edx)
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	exchange, .-exchange
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.10) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
